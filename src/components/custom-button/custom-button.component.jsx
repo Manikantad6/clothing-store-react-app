095aaf9790c3ebb6ca6f21d04a77b82ab1583460
Mani <manikanta@thoughtnext.com>
2021-01-03 22:29:47 +0530

    updated shop data with new data, updated cart utils to group items in the cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,11 @@
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
+  
 
-    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
+    <button className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
         {children}
+        { console.log('inver', inverted)}
     </button>
 
 )
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -5,8 +5,8 @@ const PreviewCollection = ({title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
         {
-            items.filter((item,idx)=> idx <4 ).map(({id, ...itemProps}) => (
-                <CollectionItem key={id} {...itemProps}></CollectionItem>
+            items.filter((item,idx)=> idx <4 ).map((item) => (
+                <CollectionItem key={item.id} item={item}></CollectionItem>
             ))
         }
         </div>
